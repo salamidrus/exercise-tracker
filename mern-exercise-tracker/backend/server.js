d6013791545466263c4b6fa5d447a0d3893f2448
salamidrus <idrussalam95gmail.com>
2021-02-16 20:59:12 +0700

    Add Route

--- mern-exercise-tracker/backend/server.js
+++ mern-exercise-tracker/backend/server.js
@@ -9,6 +9,7 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // for db
 const uri = process.env.ATLAS_URI;
@@ -23,6 +24,12 @@ connection.once("open", () => {
   // we're connected!
   console.log("MongoDB Connected!");
 });
+
+const exercisesRouter = require("./routes/exercises");
+const usersRouter = require("./routes/users");
+
+app.use("/exercises", exercisesRouter);
+app.use("/users", usersRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
